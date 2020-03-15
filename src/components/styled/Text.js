8f0080f36034d9956f328f8f2a0210b4dcf72952
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:37:22 +1000

    Create Text.js

---
+++ src/components/styled/Text.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Text = styled.div`
+  font-size: 1em;
+  line-height: 1;
+  font-weight: lighter;
+  text-transform: uppercase;
+  letter-spacing: 0.16em;
+  padding: 0.5rem 0;
+  color: #313f47;
+`
+
+export default Text
