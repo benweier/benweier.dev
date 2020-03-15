40537389151618aebbaf2602ad430e3baf037c33
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:41:09 +1000

    Create Section.js

---
+++ src/components/styled/Section.js
@@ -0,0 +1,29 @@
+import styled, { keyframes } from 'styled-components'
+
+const rotate = keyframes`
+  from {
+    transform: rotateX(15deg);
+    opacity: 0;
+  }
+
+  to {
+    transform: rotate(0);
+    opacity: 0.99;
+  }
+`
+
+const Section = styled.section`
+  transform-origin: 50% 50%;
+  animation: ${rotate} 1s ease;
+  padding: 2em 1em;
+  background: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(2px);
+  border-radius: 4px;
+  width: 100%;
+  opacity: 0.95;
+  position: relative;
+  text-align: center;
+  max-width: 23em;
+`
+
+export default Section
