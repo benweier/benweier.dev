58d77f87a591b0e15fad88545794cff42340c71b
Ben Weier <ben.weier@gmail.com>
2020-03-16 09:08:34 +1000

    Increase Text font-weight

--- src/components/styled/Text.js
+++ src/components/styled/Text.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Text = styled.div`
   font-size: 1em;
   line-height: 1;
-  font-weight: lighter;
+  font-weight: normal;
   text-transform: uppercase;
   letter-spacing: 0.16em;
   padding: 0.5rem 0;
