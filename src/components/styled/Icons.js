124031c7256f5f3dfcfa96fdfc8c434c60009e61
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:41:14 +1000

    Create Icons.js

---
+++ src/components/styled/Icons.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Icons = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  padding: 1rem;
+`
+
+export default Icons
