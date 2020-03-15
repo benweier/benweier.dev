c2c3d142dc4c0c35b26fa4ae3bb8bc8bedc485ea
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:41:16 +1000

    Create Heading.js

---
+++ src/components/styled/Heading.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Heading = styled.div`
+  font-size: 1.85em;
+  line-height: 1;
+  font-weight: lighter;
+  letter-spacing: 0.12em;
+  text-transform: uppercase;
+  padding: 0.5rem 0;
+  color: #313f47;
+`
+
+export default Heading
