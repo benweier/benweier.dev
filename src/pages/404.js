71899f7c80698893f339087d1d8284104311fce9
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:36:50 +1000

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Footer from '../components/PageFooter'
+
+const NotFound = () => (
+  <Layout>
+    <Footer />
+  </Layout>
+)
+
+export default NotFound
