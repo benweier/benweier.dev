d244bf611a3ee0f53b732c69857b8eee72ec404c
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:37:13 +1000

    Create Wrapper.js

---
+++ src/components/styled/Wrapper.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  perspective: 1000px;
+  position: relative;
+  height: 100%;
+  padding: 1.5rem;
+  z-index: 2;
+`
+
+export default Wrapper
