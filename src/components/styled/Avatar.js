b88ffaf5eb1583dfebd5335b8ecd3eaaaf473b7e
Ben Weier <ben.weier@gmail.com>
2020-03-15 16:42:04 +1000

    Create Avatar.js

---
+++ src/components/styled/Avatar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Avatar = styled.span`
+  position: relative;
+  display: inline-block;
+  line-height: 0;
+  margin: 2rem;
+  border-radius: 100%;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.08), 0 0 0 1em rgba(255, 255, 255, 0.42);
+
+  img {
+    border-radius: 100%;
+  }
+`
+export default Avatar
