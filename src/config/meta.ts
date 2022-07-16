f3289c9ba4d5348d895709b8a030c489f03abda0
Ben Weier <ben.weier@gmail.com>
2022-07-16 18:46:31 +1000

    add meta and unsplash config

---
+++ src/config/meta.ts
@@ -0,0 +1,9 @@
+export const meta = {
+  title: 'Ben Weier',
+  role: 'Developer',
+  description: 'Brisbane-based Developer specialising in React, Redux, and Next.js',
+  url: 'https://benweier.dev',
+  image: 'https://benweier.dev/preview.png',
+  site: 'benweier.dev',
+  creator: '@benweier',
+}
---
+++ src/config/unsplash.ts
@@ -0,0 +1,26 @@
+export const images = [
+  {
+    name: 'Štefan Štefančík',
+    href: 'https://unsplash.com/photos/-g7axSVst6Y',
+    src: '/unsplash/stefan-stefancik--g7axSVst6Y-unsplash.jpg',
+    alt: 'landscape photography of mountain ranges under purple and pink skies',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    name: 'Andre Benz',
+    href: 'https://unsplash.com/photos/MsMISAIe8Qw',
+    src: '/unsplash/andre-benz-MsMISAIe8Qw-unsplash.jpg',
+    alt: 'person walking on street while holding umbrella',
+    width: 1920,
+    height: 1323,
+  },
+  {
+    name: 'Wade Meng',
+    href: 'https://unsplash.com/photos/LgCj9qcrfhI',
+    src: '/unsplash/wade-meng-LgCj9qcrfhI-unsplash.jpg',
+    alt: 'gray bridge above body of water during golden hour photography',
+    width: 1920,
+    height: 1280,
+  },
+]
