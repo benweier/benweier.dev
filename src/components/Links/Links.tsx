9da121cf90fe74e6dc7e6f6d37a36e371fb26400
Ben Weier <ben.weier@gmail.com>
2022-07-17 15:14:46 +1000

    add links components

---
+++ src/components/Links/Links.tsx
@@ -0,0 +1,18 @@
+import { IconContext } from 'react-icons'
+import { FaEnvelope, FaGithub, FaTwitter } from 'react-icons/fa'
+
+export const Links = () => {
+  return (
+    <IconContext.Provider value={{ size: '28', color: 'currentColor' }}>
+      <a href="https://github.com/benweier" aria-label="GitHub" className="p-2" title="GitHub">
+        <FaGithub />
+      </a>
+      <a href="https://twitter.com/benweier" aria-label="Twitter" className="p-2" title="Twitter">
+        <FaTwitter />
+      </a>
+      <a href="mailto:web@benweier.dev" aria-label="Email" className="p-2" title="Email">
+        <FaEnvelope />
+      </a>
+    </IconContext.Provider>
+  )
+}
