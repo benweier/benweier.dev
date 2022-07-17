f3e51728db86a34e6eecbb88b45af10415ff8578
Ben Weier <ben.weier@gmail.com>
2022-07-17 15:15:00 +1000

    add background image component

---
+++ src/components/BackgroundImage/BackgroundImage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/future/image'
+import { images } from '@/config/unsplash'
+
+interface BackgroundImageProps {
+  image: typeof images[number]
+}
+
+export const BackgroundImage = ({ image }: BackgroundImageProps) => {
+  return (
+    <Image
+      src={image.src}
+      alt={image.alt}
+      width={image.width}
+      height={image.height}
+      className="absolute inset-0 h-full max-h-full w-full max-w-full object-cover opacity-40 blur-sm"
+    />
+  )
+}
