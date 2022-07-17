123e1b92a059361f4d655705fcafa514ab55cd2f
Ben Weier <ben.weier@gmail.com>
2022-07-17 15:14:39 +1000

    add photo attribution component

---
+++ src/components/PhotoAttribution/PhotoAttribution.tsx
@@ -0,0 +1,20 @@
+import { images } from '@/config/unsplash'
+
+interface PhotoAttributionProps {
+  image: typeof images[number]
+}
+
+export const PhotoAttribution = ({ image }: PhotoAttributionProps) => {
+  return (
+    <div>
+      Photo by{' '}
+      <a className="underline" href={image.href}>
+        {image.name}
+      </a>{' '}
+      on{' '}
+      <a className="underline" href="https://unsplash.com">
+        Unsplash
+      </a>
+    </div>
+  )
+}
