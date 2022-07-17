666cb3013f16841c98ca41fabc064f0f0c88a05f
Ben Weier <ben.weier@gmail.com>
2022-07-17 15:14:52 +1000

    add avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/future/image'
+import avatar from '@/images/avatar.png'
+
+export const Avatar = () => {
+  return (
+    <Image
+      src={avatar}
+      alt="Stylised avatar of Ben Weier"
+      width={132}
+      height={132}
+      className="rounded-full shadow-[0_0_10px_-5px_rgba(0,0,0,0.4)]"
+      placeholder="blur"
+      sizes="132px"
+      loading="eager"
+    />
+  )
+}
