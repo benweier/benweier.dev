a8bcb2a541e72bb37e4502281373d94189c24c86
Ben Weier <ben.weier@gmail.com>
2022-07-19 18:10:40 +1000

    use typeof mount from cypress react

--- cypress/cypress.d.ts
+++ cypress/cypress.d.ts
@@ -1,14 +1,9 @@
-import { MountOptions, MountReturn } from 'cypress/react'
+import { mount } from 'cypress/react'
 
 declare global {
   namespace Cypress {
     interface Chainable {
-      /**
-       * Mounts a React node
-       * @param component React Node to mount
-       * @param options Additional options to pass into mount
-       */
-      mount(component: React.ReactNode, options?: MountOptions): Cypress.Chainable<MountReturn>
+      mount: typeof mount
     }
   }
 }
