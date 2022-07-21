c8307a0098ab826c0ff4f76140fe576aad2f80a8
Ben Weier <ben.weier@gmail.com>
2022-07-21 19:09:37 +1000

    add home page e2e spec

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,20 @@
+context('benweier.dev', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('renders an avatar', () => {
+    cy.get('main').findByAltText('Stylised avatar of Ben Weier').should('exist')
+  })
+
+  it('renders text content', () => {
+    cy.get('main').findByText('Ben Weier').should('exist')
+    cy.get('main').findByText('Developer').should('exist')
+  })
+
+  it('renders icon links', () => {
+    cy.get('main').findByTitle('GitHub').should('exist').should('have.attr', 'href', 'https://github.com/benweier')
+    cy.get('main').findByTitle('Twitter').should('exist').should('have.attr', 'href', 'https://twitter.com/benweier')
+    cy.get('main').findByTitle('Email').should('exist').should('have.attr', 'href', 'mailto:web@benweier.dev')
+  })
+})
