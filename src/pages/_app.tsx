cdc4949357a5eadae67029a880d1afe462d76dca
Ben Weier <ben.weier@gmail.com>
2022-07-23 16:51:14 +1000

    add default seo component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '@/styles/tailwind.css'
+import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
+import { meta } from '@/config/meta'
+import '@/styles/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title={`${meta.name} :: ${meta.role}`}
+        description={meta.description}
+        openGraph={{
+          type: 'website',
+          title: meta.name,
+          description: meta.description,
+          url: meta.url,
+          images: [{ url: meta.image }],
+          site_name: meta.site,
+        }}
+        twitter={{
+          handle: meta.creator,
+          cardType: 'summary',
+          site: meta.creator,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
