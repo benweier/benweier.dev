1f9136ce86e6e7dfc387522b10d4eaca64c4448b
Ben Weier <ben.weier@gmail.com>
2022-07-24 18:17:43 +1000

    add links component test

---
+++ src/components/Links/Links.cy.tsx
@@ -0,0 +1,11 @@
+import { Links } from './Links'
+
+describe('Links', () => {
+  it('should display a list of links', () => {
+    cy.mount(<Links />)
+
+    cy.get('[title="GitHub"]').should('have.attr', 'href', 'https://github.com/benweier')
+    cy.get('[title="Twitter"]').should('have.attr', 'href', 'https://twitter.com/benweier')
+    cy.get('[title="Email"]').should('have.attr', 'href', 'mailto:web@benweier.dev')
+  })
+})
