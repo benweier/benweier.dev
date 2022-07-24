9c13c10f18b44445673d5bdbddfa5cb6e9a92f2b
Ben Weier <ben.weier@gmail.com>
2022-07-24 19:06:41 +1000

    update component spec pattern, remove e2e empty setup node events

--- cypress.config.js
+++ cypress.config.js
@@ -6,14 +6,12 @@ module.exports = defineConfig({
   video: false,
 
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(_on, _config) {
-      //
-    },
   },
 
   component: {
-    specPattern: 'src/**/*.cy.tsx',
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
     devServer: {
       framework: 'next',
       bundler: 'webpack',
