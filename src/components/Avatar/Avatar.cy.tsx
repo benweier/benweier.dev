87201bdeb39d6071995c38312992952eb878fa63
Ben Weier <ben.weier@gmail.com>
2022-07-24 18:16:44 +1000

    add avatar component test

---
+++ src/components/Avatar/Avatar.cy.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from './Avatar'
+
+describe('Avatar', () => {
+  it('should render aan avatar image', () => {
+    cy.mount(<Avatar />)
+
+    cy.get('#avatar').should('have.attr', 'src')
+    cy.get('#avatar').should('have.attr', 'alt')
+    cy.get('#avatar').should('have.attr', 'width')
+    cy.get('#avatar').should('have.attr', 'height')
+  })
+})
