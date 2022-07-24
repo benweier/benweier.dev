93e1ef4bd0642479a3c5d30a2ee736e4fe08f7b7
Ben Weier <ben.weier@gmail.com>
2022-07-24 23:49:18 +1000

    add i18n config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const config = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en-US', 'en-GB', 'en-AU'],
+    defaultLocale: 'en-US',
+  },
   images: {
     imageSizes: [132],
     deviceSizes: [380, 640, 768, 1024, 1200],
