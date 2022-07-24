4a77ccfa779c71d5529e0a82a6be155438bda8bf
Ben Weier <ben.weier@gmail.com>
2022-07-24 23:53:02 +1000

    add next intl provider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { NextIntlProvider } from 'next-intl'
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import { meta } from '@/config/meta'
@@ -6,24 +7,26 @@ import '@/styles/tailwind.css'
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <DefaultSeo
-        title={`${meta.name} :: ${meta.role}`}
-        description={meta.description}
-        openGraph={{
-          type: 'website',
-          title: meta.name,
-          description: meta.description,
-          url: meta.url,
-          images: [{ url: meta.image }],
-          site_name: meta.site,
-        }}
-        twitter={{
-          handle: meta.creator,
-          cardType: 'summary',
-          site: meta.creator,
-        }}
-      />
-      <Component {...pageProps} />
+      <NextIntlProvider messages={pageProps.messages}>
+        <DefaultSeo
+          title={`${meta.name} :: ${meta.role}`}
+          description={meta.description}
+          openGraph={{
+            type: 'website',
+            title: meta.name,
+            description: meta.description,
+            url: meta.url,
+            images: [{ url: meta.image }],
+            site_name: meta.site,
+          }}
+          twitter={{
+            handle: meta.creator,
+            cardType: 'summary',
+            site: meta.creator,
+          }}
+        />
+        <Component {...pageProps} />
+      </NextIntlProvider>
     </>
   )
 }
