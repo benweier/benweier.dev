4fbad7ea2a293e586e34a7a3427d4fdca75a8e99
Ben Weier <ben.weier@gmail.com>
2022-07-27 19:42:57 +1000

    removed fixed lang attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 const Document = () => {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
         <meta name="theme-color" content="#000000" />
 
