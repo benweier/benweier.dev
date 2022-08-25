dfa5f12e682b6602b6b9bf099b55bd078c6fe653
Ben Weier <ben.weier@gmail.com>
2022-08-25 10:49:57 +1000

    replace react icons with hero icons and tweak sizing

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,22 +1,21 @@
-import { IconContext } from 'react-icons'
-import { FaEnvelope, FaGithub, FaTwitter } from 'react-icons/fa'
+import AtSymbolIcon from '@heroicons/react/24/outline/AtSymbolIcon'
+import CodeBracketIcon from '@heroicons/react/24/outline/CodeBracketIcon'
+import EnvelopeIcon from '@heroicons/react/24/outline/EnvelopeIcon'
 import { Anchor } from '@/components/Anchor'
 import styles from './styles.module.css'
 
 export const Links = () => {
   return (
     <div id="links" className="grid grid-flow-col items-center justify-between gap-8">
-      <IconContext.Provider value={{ size: '28', color: 'currentColor' }}>
-        <Anchor href="https://github.com/benweier" aria-label="GitHub" className={styles.link} title="GitHub">
-          <FaGithub />
-        </Anchor>
-        <Anchor href="https://twitter.com/benweier" aria-label="Twitter" className={styles.link} title="Twitter">
-          <FaTwitter />
-        </Anchor>
-        <Anchor href="mailto:web@benweier.dev" aria-label="Email" className={styles.link} title="Email">
-          <FaEnvelope />
-        </Anchor>
-      </IconContext.Provider>
+      <Anchor href="https://github.com/benweier" aria-label="GitHub" className={styles.link} title="GitHub">
+        <CodeBracketIcon className="h-8 w-8" />
+      </Anchor>
+      <Anchor href="https://twitter.com/benweier" aria-label="Twitter" className={styles.link} title="Twitter">
+        <AtSymbolIcon className="h-8 w-8" />
+      </Anchor>
+      <Anchor href="mailto:web@benweier.dev" aria-label="Email" className={styles.link} title="Email">
+        <EnvelopeIcon className="h-8 w-8" />
+      </Anchor>
     </div>
   )
 }
